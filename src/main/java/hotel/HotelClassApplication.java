67a21cdbccc8a65dc2699452a662953f49b32c5a
Nguyễn Gia Khánh <122737548+KhanhKube@users.noreply.github.com>
2025-09-18 12:41:04 +0700

    Init project

---
+++ src/main/java/hotel/HotelClassApplication.java
@@ -0,0 +1,13 @@
+package hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelClassApplication.class, args);
+	}
+
+}
