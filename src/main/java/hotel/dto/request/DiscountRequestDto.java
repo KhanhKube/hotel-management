03613a1a8c063f170387f2880c29981ac6980490
alone nugget <fakeymeo@gmail.com>
2025-09-28 21:43:00 +0700

    Discount-Respository-missing constuc

--- src/main/java/hotel/dto/request/DiscountRequestDto.java
+++ src/main/java/hotel/dto/request/DiscountRequestDto.java
@@ -4,6 +4,11 @@ public class DiscountRequestDto {
     private String code;
     private Long orderId;
 
+    public DiscountRequestDto(String code, Long orderId) {
+        this.code = code;
+        this.orderId = orderId;
+    }
+
     public String getCode() {
         return code;
     }
