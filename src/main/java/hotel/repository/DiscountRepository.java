fc479ff005cbd4070888939129ef730a98b6f677
alone nugget <fakeymeo@gmail.com>
2025-09-28 21:37:54 +0700

    Discount-Respository

--- src/main/java/hotel/repository/DiscountRepository.java
+++ src/main/java/hotel/repository/DiscountRepository.java
@@ -1,5 +1,6 @@
 package hotel.repository;
 
+import hotel.db.entity.Discount;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 public interface DiscountRepository extends JpaRepository<Discount, Long> {
