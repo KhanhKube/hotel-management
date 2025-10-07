2eff3188b58190fc8919a4359b5d0ecf65bea13e
HieuNonTay <hieubuivan646@gmail.com>
2025-10-07 19:22:38 +0700

    receptionist

---
+++ src/main/java/hotel/rest/receptionist/ReceptionistController.java
@@ -0,0 +1,13 @@
+package hotel.rest.receptionist;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/hotel-management/receptionist")
+@RequiredArgsConstructor
+@Slf4j
+public class ReceptionistController {
+}
