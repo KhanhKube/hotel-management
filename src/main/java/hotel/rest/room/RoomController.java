72bd2563af7970040148bd7f4cba2289eb69f4cb
alone nugget <fakeymeo@gmail.com>
2025-10-18 15:15:12 +0700

    RoomList-ManagementScreen

--- src/main/java/hotel/rest/room/RoomController.java
+++ src/main/java/hotel/rest/room/RoomController.java
@@ -7,38 +7,33 @@ import hotel.service.room.RoomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@RestController
-@RequestMapping("/api/rooms")
+@Controller
 @RequiredArgsConstructor
+@RequestMapping("/hotel-management/room")
 public class RoomController {
-//    private final RoomRepository roomRepo;
-//
-//    @GetMapping
-//    public List<Room> list(@RequestParam(required = false) RoomStatus status) {
-//        return status==null ? roomRepo.findAll() : roomRepo.findByStatus(status);
-//    }
-//
-//    @PatchMapping("/{id}/status")
-//    public Room changeStatus(@PathVariable Long id, @RequestParam RoomStatus status) {
-//        Room r = roomRepo.findById(id).orElseThrow();
-//        r.setStatus(status);
-//        return roomRepo.save(r);
-//    }
 
 	private final BookingService bookingService;
 	private final RoomService roomService;
+
+    @GetMapping
+    public String view(Model model) {
+        model.addAttribute("listRoom", roomService.getRoomList());
+        return "management/room/room-management";
+    }
 
 	@PostMapping("/search")
 	public ListRoomResponse listRooms(@RequestBody SearchRoomRequest request) {
 		return bookingService.listRoom(request);
 	}
 
-	@GetMapping
+	@GetMapping("/api/list")
 	public ResponseEntity<Map<String, Object>> getRooms() {
 		ListRoomResponse rooms = roomService.getAllRoomForSearch();
 		Map<String, Object> response = new HashMap<>();
