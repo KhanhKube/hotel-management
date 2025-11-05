6ad495439c3bf2914b99b8752d3f906cd7e7a3d1
alone nugget <fakeymeo@gmail.com>
2025-11-05 23:37:46 +0700

    Add view list view room

--- src/main/java/hotel/db/repository/roomview/RoomViewRepository.java
+++ src/main/java/hotel/db/repository/roomview/RoomViewRepository.java
@@ -2,8 +2,16 @@ package hotel.db.repository.roomview;
 
 import hotel.db.entity.RoomView;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface RoomViewRepository extends JpaRepository<RoomView, Integer> {
+
+    //Lấy các Id view của id phòng.
+    @Query(value = "SELECT view_id FROM room_views WHERE room_id = :roomId AND is_deleted = 0", nativeQuery = true)
+    List<Integer> findRoomViewId(@Param("roomId") Integer roomId);
 }
