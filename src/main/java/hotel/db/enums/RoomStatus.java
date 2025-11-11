05e5cc0e190d13c3ade4e697ef3e1e51b4eacba3
alone nugget <fakeymeo@gmail.com>
2025-11-11 20:34:02 +0700

    Add Maintenance room Time

--- src/main/java/hotel/db/enums/RoomStatus.java
+++ src/main/java/hotel/db/enums/RoomStatus.java
@@ -4,8 +4,9 @@ public class RoomStatus {
     public static final String AVAILABLE = "Đang trống";
     public static final String OCCUPIED = "Đang thuê";
     public static final String CLEANING = "Đang dọn dẹp";
+    public static final String MAINTENANCE = "Đang bảo trì";
     
     public static final String[] ALL = {
-        AVAILABLE, OCCUPIED, CLEANING
+        AVAILABLE, OCCUPIED, CLEANING, MAINTENANCE
     };
 }
